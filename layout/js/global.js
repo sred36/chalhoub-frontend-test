a6e44666d20fc8c3d4756117d346159c10da4284
sred36 <sankarsiva018@gmail.com>
2021-06-01 11:31:04 +0530

    feat(PDP-STEP2) - PDP Page Data from  AJAX

--- layout/js/global.js
+++ layout/js/global.js
@@ -1,11 +1,5 @@
-    var product = [
-        {img: "images/1.png", title: "Twon headd", price: "AED: 300",sizes: "Available Sizes : S M",desc:"Calendula Herbal Extract Alcohol-Free Toner"},
-        {img: "images/6.png", title: "Bangalore tshirt", price: "AED: 400", sizes: "Available Sizes : L XL", desc:"-Moisten a sterile cotton pad with facial toner, and apply to skin in areas where needed, avoiding the eye area        "},
-        {img: "images/2.png", title: "Candian  tshirt", price: "AED: 500", sizes: "Available Sizes : S M XL XXL ", desc:"ntains no alcohol or harsh synthetic drying agent"},
-        {img: "images/4.png", title: "leather Hoodie", price: "AED: 600", sizes: "Available Sizes : L  XL ", desc:"The calendula petals in our unique formula are still inserted by hand in the Kiehl’s apothecary tradition to"}
-    ];
-
      var $cache = {};
+     var productData;
     function initializeCache() {
         $cache.document = $(document);
         $cache.color = $('.product__colors li');
@@ -16,6 +10,7 @@
         $cache.productPrice =  $cache.productDetails.find('.price');
         $cache.productAvailbilSize =  $cache.productDetails.find('.prouct__availabity');
         $cache.productDesc =  $cache.productDetails.find('.product__description--content');
+        $cache.seeMore =  $('.product__seemore--gallary li img');
         
 
     
@@ -35,11 +30,13 @@
     }
     
     function AppendProductDetails(source) {
-        product.filter(function(element) {
-                 if(element.img === source ) {
-                     appendDataToDom(element);
-                 }
-        });
+        if(productData) {
+            productData.filter(function(element) {
+                    if(element.img === source ) {
+                        appendDataToDom(element);
+                    }
+            });
+        }
         
         
     }
@@ -49,14 +46,39 @@
         $cache.productTitle.text(element.title)
         $cache.productPrice.text(element.price)
         $cache.productAvailbilSize.text(element.sizes)
-        $cache.productDesc.text(element.desc)
+        $cache.productDesc.text(element.desc);
+        updateSeeMoreData(element.seemore);
 
         
 
     }
 
+    function updateSeeMoreData(seemoreData) {
+        seemoreData.map(function(element,index) {
+            $($cache.seeMore[index]).attr('src', element)
+        });
+
+    }
+
+    function loadJSONData() {
+       var API = "https://api.jsonbin.io/b/60b5c741b104de5acddb7fcf";
+       $.ajax({
+           type: "GET",
+           url: API, 
+           dataType: "json",
+        success: function(result){
+            productData = result;
+        },
+        error: function(response) {
+            console.log(response);
+        }
+        
+    });
+   
+}
 $(document).ready(function() {
     initializeCache();
     initializeEvents();
+    loadJSONData();
            
 });
